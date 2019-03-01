787b93d7fa790df6cf2805c83709fcf48ed3d7ec
ahribori <ahribori@gmail.com>
2019-03-02 00:09:53 +0900

    Notification 권한 획득 + notification 트리거

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,26 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        // 서비스워커를 register 하면 promise를 반환한다.
-        navigator.serviceWorker
-            .register('/sw.js', { scope: '/' })
-            .then(() => {
-                console.log('서비스 워커가 등록되었다.');
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    });
+  window.addEventListener('load', function() {
+    // 서비스워커를 register 하면 promise를 반환한다.
+    navigator.serviceWorker
+      .register('/sw.js', { scope: '/' })
+      .then(registration => {
+        console.log(registration);
+        console.log('서비스 워커가 등록되었다.');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    document
+      .getElementById('notification_trigger')
+      .addEventListener('click', () => {
+        Notification.requestPermission().then(function(result) {
+          const noti = new Notification('Title', {
+            body: 'Body',
+            icon: '../images/ryan.jpg',
+          });
+          console.log(noti);
+        });
+      });
+  });
 }
