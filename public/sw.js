9ff4e3e4c0382836b6331021093271a6282bc6fc
ahribori <ahribori@gmail.com>
2019-03-03 01:44:06 +0900

    cache & fetch

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,20 @@
-self.addEventListener('install', e => {
-    console.log('install', e);
+self.addEventListener('install', event => {
+  console.log('install', event);
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll(['/images/ryan.jpg']);
+    }),
+  );
 });
 
-self.addEventListener('activate', e => {
-    console.log('activate', e);
+self.addEventListener('activate', event => {
+  console.log('activate', event);
 });
 
-self.addEventListener('fetch', e => {
-    console.log('fetch', e);
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }),
+  );
 });
